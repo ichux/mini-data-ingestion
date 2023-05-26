101f3d3e6f5621f35692ae17ab2ae6c0e066ff89
Chukwudi Nwachukwu <theichux@gmail.com>
2023-05-26 14:11:45 +0100

    removed unwanted region

--- mdi/app/__init__.py
+++ mdi/app/__init__.py
@@ -3,7 +3,7 @@ import logging
 from flask import Flask
 from flask_appbuilder import SQLA, AppBuilder
 
-from ..wikidata import load_wikidata
+
 
 """
  Logging configuration
@@ -18,21 +18,8 @@ db = SQLA(app)
 appbuilder = AppBuilder(app, db.session)
 
 
-"""
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-#Only include this for SQLLite constraints
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    # Will force sqllite contraint foreign keys
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-"""
-
 from . import views
-
+from ..wikidata import load_wikidata
 
 @app.cli.command("data")
 def load_wikidata_command():
