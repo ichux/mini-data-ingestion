f4977bd96514a54b0adfef0cbfaa8eee43b7752e
Chukwudi Nwachukwu <theichux@gmail.com>
2023-05-26 14:22:51 +0100

    did some essential formats

--- mdi/app/__init__.py
+++ mdi/app/__init__.py
@@ -3,11 +3,7 @@ import logging
 from flask import Flask
 from flask_appbuilder import SQLA, AppBuilder
 
-
-
-"""
- Logging configuration
-"""
+from ..wikidata import load_wikidata
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
@@ -19,7 +15,7 @@ appbuilder = AppBuilder(app, db.session)
 
 
 from . import views
-from ..wikidata import load_wikidata
+
 
 @app.cli.command("data")
 def load_wikidata_command():
